306c5d0a0ead5dbbcb7d3b6e4ce4f6f016cf4fd5
luisacicolini <luisacicolini@gmail.com>
2025-10-30 08:24:23 +0000

    addiw docstring

--- RISCV/Instructions.lean
+++ RISCV/Instructions.lean
@@ -2,5 +2,11 @@
 
 namespace RV64I
 
+/--
+  Adds the sign-extended 12-bit immediate to register rs1 and produces the proper sign-extension
+  of a 32-bit result in rd. Overflows are ignored and the result is the low 32 bits of the result
+  sign-extended to 64 bits. Note, ADDIW rd, rs1, 0 writes the sign-extension of the lower 32 bits
+  of register rs1 into register rd (assembler pseudoinstruction SEXT.W).
+-/
 def addiw (imm : BitVec 12) (rs1_val : BitVec 64) : BitVec 64 :=
   BitVec.signExtend 64 (BitVec.setWidth 32 (BitVec.add (BitVec.signExtend 64 imm) rs1_val))
