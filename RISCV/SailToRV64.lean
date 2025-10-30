4f8d31066a03c0976478e1ba54cec84a037fe670
luisacicolini <luisacicolini@gmail.com>
2025-10-30 08:04:41 +0000

    chore: correct namespaces

--- RISCV/SailToRV64.lean
+++ RISCV/SailToRV64.lean
@@ -71,5 +71,5 @@ def skeleton_UTYPE_LUI  (imm : BitVec 20) (rd : regidx)
 
 
 theorem add_eq (imm : BitVec 12) (rs1 : regidx) (rd : regidx) :
-    execute_ADDIW (imm) (rs1) (rd) = skeleton_unary rs1 rd (RV64I.addiw imm) := by
-  simp [execute_ADDIW, skeleton_unary, RV64I.addiw]
+    execute_ADDIW (imm) (rs1) (rd) = skeleton_unary rs1 rd (SailRV64I.addiw imm) := by
+  simp [execute_ADDIW, skeleton_unary, SailRV64I.addiw]
