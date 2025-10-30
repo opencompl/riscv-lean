7e74eff69a541df7e647119c8c08461e0eca19b2
luisacicolini <luisacicolini@gmail.com>
2025-10-30 15:58:32 +0000

    choreL sailtopure

--- RISCV/SailPureToInstructions.lean
+++ RISCV/SailPureToInstructions.lean
@@ -23,6 +23,14 @@ theorem utype_auipc_eq (imm : BitVec 20) (pc : BitVec 64) :
   unfold instHPowInt_leanRV64D
   bv_decide
 
+theorem addiw_eq (imm : BitVec 12) (rs1_val : BitVec 64) :
+    SailRV64I.addiw imm rs1_val = RV64I.addiw imm rs1_val := by
+  simp only [SailRV64I.addiw, LeanRV64D.Functions.sign_extend, Sail.BitVec.signExtend, Nat.sub_zero,
+    Nat.reduceAdd, Sail.BitVec.extractLsb, RV64I.addiw, BitVec.add_eq]
+  rw [BitVec.extractLsb, BitVec.setWidth_eq_extractLsb' (by omega)]
+  unfold instHPowInt_leanRV64D
+  bv_decide
+
 theorem shiftiop_slli_eq (shamt : BitVec 6) (rs1_val : BitVec 64) :
     SailRV64I.shiftiop shamt sop.SLLI rs1_val = slli shamt rs1_val := by
   simp [SailRV64I.shiftiop, Sail.shift_bits_left, slli]
@@ -40,14 +48,6 @@ theorem shiftiop_srai_eq (shamt : BitVec 6) (rs1_val : BitVec 64) :
     BitVec.signExtend_eq]
   rfl
 
-theorem addiw_eq (imm : BitVec 12) (rs1_val : BitVec 64) :
-    SailRV64I.addiw imm rs1_val = RV64I.addiw imm rs1_val := by
-  simp only [SailRV64I.addiw, LeanRV64D.Functions.sign_extend, Sail.BitVec.signExtend, Nat.sub_zero,
-    Nat.reduceAdd, Sail.BitVec.extractLsb, RV64I.addiw, BitVec.add_eq]
-  rw [BitVec.extractLsb, BitVec.setWidth_eq_extractLsb' (by omega)]
-  unfold instHPowInt_leanRV64D
-  bv_decide
-
 theorem shiftiwop_slliw_eq (shamt : BitVec 5) (rs1_val : BitVec 64) :
     SailRV64I.shiftiwop shamt sopw.SLLIW rs1_val = slliw shamt rs1_val := by
   simp only [SailRV64I.shiftiwop, LeanRV64D.Functions.sign_extend, Sail.BitVec.signExtend,
@@ -63,8 +63,12 @@ theorem shiftiwop_srliw_eq (shamt : BitVec 5) (rs1_val : BitVec 64) :
   bv_decide
 
 theorem shiftiwop_sraiw_eq (shamt : BitVec 5) (rs1_val : BitVec 64) :
-    SailRV64I.shiftiwop shamt sopw.SRAIW rs1_val = sraiw shamt rs1_val := by rfl
-
+    SailRV64I.shiftiwop shamt sopw.SRAIW rs1_val = sraiw shamt rs1_val := by
+  simp only [SailRV64I.shiftiwop, LeanRV64D.Functions.sign_extend, Sail.BitVec.signExtend,
+    LeanRV64D.Functions.shift_bits_right_arith, LeanRV64D.Functions.shift_right_arith,
+    Sail.BitVec.extractLsb, Int.cast_ofNat_Int, Int.reduceSub, sraiw, Nat.sub_zero, Nat.reduceAdd,
+    BitVec.sshiftRight_eq', sshiftRight_eq_setWidth_extractLsb_signExtend, Nat.add_one_sub_one]
+  rfl
 theorem rtypew_addw_eq (rs1_val : BitVec 64) (rs2_val : BitVec 64) :
     SailRV64I.rtypew ropw.ADDW rs1_val rs2_val = addw rs1_val rs2_val := by
   simp only [SailRV64I.rtypew, LeanRV64D.Functions.sign_extend, Sail.BitVec.signExtend,
