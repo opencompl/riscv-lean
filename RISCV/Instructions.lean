c45290aaf50e6559f803617612d4858cd70b1a8d
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2025-10-30 15:31:24 +0000

    chore: accept whitespace sugg
    Co-authored-by: Tobias Grosser <tobias@grosser.es>

--- RISCV/Instructions.lean
+++ RISCV/Instructions.lean
@@ -33,7 +33,7 @@ def srli (shamt : BitVec 6) (rs1_val : BitVec 64) : BitVec 64 := rs1_val >>> sha
   Performs arithmetic right shift on the value in register rs1 by the shift amount held in the
   lower 5 bits of the immediate In RV64, bit-25 is used to shamt[5].
 -/
-def srai (shamt : BitVec 6) (rs1_val : BitVec 64): BitVec 64 :=
+def srai (shamt : BitVec 6) (rs1_val : BitVec 64) : BitVec 64 :=
   BitVec.signExtend 64 (BitVec.sshiftRight' rs1_val shamt)
 
 /--
