1c09ac682027eb713fa344a99eed25e881ab5a6e
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2025-10-31 09:22:08 +0000

    chore: rename `rtype_*` theorems in `SailToRV64` to `rtypew_*`
    We correct the names of `rtype_*` theorems, as they deal with `RTYPEW` operations (not `RTYPE`).

--- RISCV/SailToRV64.lean
+++ RISCV/SailToRV64.lean
@@ -76,31 +76,31 @@ theorem shiftiwop_sraiw_eq (shamt : BitVec 5) (rs1 : regidx) (rd : regidx) :
     skeleton_unary]
   rfl
 
-theorem rtype_add_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
+theorem rtypew_add_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
     execute_RTYPEW rs2 rs1 rd ropw.ADDW
       = skeleton_binary rs2 rs1 rd (fun val1 val2 => SailRV64I.rtypew ropw.ADDW val2 val1) := by
   simp [execute_RTYPEW, skeleton_binary]
   rfl
 
-theorem rtype_sub_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
+theorem rtypew_sub_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
     execute_RTYPEW rs2 rs1 rd ropw.SUBW
       = skeleton_binary rs2 rs1 rd (fun val1 val2 => SailRV64I.rtypew ropw.SUBW val2 val1) := by
   simp [execute_RTYPEW, skeleton_binary]
   rfl
 
-theorem rtype_sllw_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
+theorem rtypew_sllw_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
     execute_RTYPEW rs2 rs1 rd ropw.SLLW
       = skeleton_binary rs2 rs1 rd (fun val1 val2 => SailRV64I.rtypew ropw.SLLW val2 val1) := by
   simp [execute_RTYPEW, skeleton_binary]
   rfl
 
-theorem rtype_srlw_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
+theorem rtypew_srlw_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
     execute_RTYPEW rs2 rs1 rd ropw.SRLW
       = skeleton_binary rs2 rs1 rd (fun val1 val2 => SailRV64I.rtypew ropw.SRLW val2 val1) := by
   simp [execute_RTYPEW, skeleton_binary]
   rfl
 
-theorem rtype_sraw_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
+theorem rtypew_sraw_eq (rs1 : regidx) (rs2 : regidx) (rd : regidx) :
     execute_RTYPEW rs2 rs1 rd ropw.SRAW
       = skeleton_binary rs2 rs1 rd (fun val1 val2 => SailRV64I.rtypew ropw.SRAW val2 val1) := by
   simp [execute_RTYPEW, skeleton_binary]
